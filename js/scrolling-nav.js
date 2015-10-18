318af4151ca46ca0314b22a758836bbf2e1fe521
ArnoChen <a57432739@yahoo.com.tw>
2015-10-19 01:08:33 +0800

    Revert "test20151019"
    This reverts commit f6e36f6bf9d97ee4773e8a364dc807e4025ffb64.

--- js/scrolling-nav.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('a[href*=#]:not([href=#])').click(function() {
-	var target = $(this.hash);
-	$('html,body').animate({
-	scrollTop: target.offset().top }, 400 ); 
-	return false;
-	});
-	});
